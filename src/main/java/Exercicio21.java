e528cb1c4200f8bd8c41e0d8d016ba8fb926b07f
ADM <ADM@DESKTOP-88AP3BG>
2024-09-18 15:30:51 -0300

    init: início do projeto

---
+++ src/main/java/Exercicio21.java
@@ -0,0 +1,11 @@
+public class Exercicio21 {
+
+    public static void main(String[] args) {
+        System.out.println("Números pares do entre 33 e 57: ");
+        for(int i =33;i<57;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
